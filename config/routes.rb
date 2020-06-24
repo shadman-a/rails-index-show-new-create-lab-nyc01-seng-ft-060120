b5167f582f901d6bb18c0e362f82ca8c37ac7be0
shadman-a <shadman@outlook.com>
2020-06-24 09:14:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'coupons/index'
+
+  get 'coupons/create'
+
+  get 'coupons/new'
+
+  get 'coupons/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +61,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :coupons, only: [:index, :show, :create, :new]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "ABC", store: "Homey")
+Coupon.create(coupon_code: "25OFFNOW", store: "Tarmart")
+Coupon.create(coupon_code: "PIZZA", store: "Dominate")
